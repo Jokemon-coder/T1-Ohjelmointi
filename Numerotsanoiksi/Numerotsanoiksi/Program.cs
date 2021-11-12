070bc748c331d3d232b2aa6e146ef4c32ba62306
Jokemon-coder <Jokemon-coder@users.noreply.github.com>
2021-11-12 12:21:45 +0200

    Numerotsanoiksi

---
+++ Numerotsanoiksi/Numerotsanoiksi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numerotsanoiksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
